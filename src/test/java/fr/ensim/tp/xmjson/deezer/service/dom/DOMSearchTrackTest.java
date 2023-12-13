dc0e99dfa13a6ba7fa9accc5380638f4f768f256
e2205046 <andr1mpn@gmail.com>
2023-12-13 18:05:49 +0100

    TP progression

--- src/test/java/fr/ensim/tp/xmjson/deezer/service/dom/DOMSearchTrackTest.java
+++ src/test/java/fr/ensim/tp/xmjson/deezer/service/dom/DOMSearchTrackTest.java
@@ -14,12 +14,13 @@ class DOMSearchTrackTest {
     void readTracks() throws Exception {
         InputStream in = getClass().getResourceAsStream("/data/album-6332024.xml");
 
-        DOMSearchTrack dom = new DOMSearchTrack();
-        List<Track> TrackList = dom.readTracks(in);
+        DOMSearchTrack domSearchTrack = new DOMSearchTrack();
+        List<Track> trackList = domSearchTrack.readTracks(in);
 
-        assertNotNull(TrackList);
-        assertEquals("(Druun)", TrackList.get(0).getTitle());
-        assertEquals("https://cdns-preview-5.dzcdn.net/stream/c-5f6892fc926df78cbe81cb5e308f9f8e-3.mp3", TrackList.get(0).getPreview());
+        assertNotNull(trackList);
+        assertEquals(13, trackList.size());
+        assertEquals("(Druun)", trackList.get(0).getTitle());
+        assertEquals("https://cdns-preview-5.dzcdn.net/stream/c-5f6892fc926df78cbe81cb5e308f9f8e-3.mp3", trackList.get(0).getPreview());
     }
 }
 
